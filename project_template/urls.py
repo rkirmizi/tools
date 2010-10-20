f7b058ced3ddde52b8d00f040ffefbf35301db78
Recep KIRMIZI <rkirmizi@gmail.com>
2010-10-20 12:33:17 +0300

    css fix

--- project_template/urls.py
+++ project_template/urls.py
@@ -1,9 +1,15 @@
 from django.conf.urls.defaults import *
-
+from django.conf import settings
 #from django.contrib import admin
 #admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'home.html'}),
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+	    {
+	    	'document_root': settings.MEDIA_ROOT,
+			'show_indexes': True
+		}),
+
 #    (r'^admin/', include(admin.site.urls)),
 )
